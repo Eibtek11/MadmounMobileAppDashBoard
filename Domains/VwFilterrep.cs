4e2a5891b9c6dd6f402df81d86ca97128de91782
ahmedmostafa706@gmail.com <ahmedmostafa706@gmail.com>
2023-01-01 07:01:37 -0800

    new remote

---
+++ Domains/VwFilterrep.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domains
+{
+    public class VwFilterrep
+    {
+        public Guid SrRepServiceId { get; set; }
+        public Guid? ServiceId { get; set; }
+        public string Id { get; set; }
+        
+        public Guid? CityId { get; set; }
+      
+        public Guid? AreaId { get; set; }
+       
+
+    }
+}
---
+++ MadmounMobileApp/Models/ServicesRequiredViewPageModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MadmounMobileApp.Models
+{
+    public class ServicesRequiredViewPageModel
+    {
+        public string ServiceSyntax { get; set; }
+
+        public string SrRepId { get; set; }
+
+        public string SrReqId { get; set; }
+        public Guid? ServiceId { get; set; }
+
+
+        public string CreatedBy { get; set; }
+
+        public string Notes { get; set; }
+    }
+}
